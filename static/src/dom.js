d12ee0f8053a324d73748b12b090e523d87cb017
noheartpen <3104408599@qq.com>
2024-09-11 21:11:58 +0900

    feat: 双击查词

--- static/src/dom.js
+++ static/src/dom.js
@@ -1,4 +1,228 @@
 // DOM相关操作
+/**
+ * 获取光标处附近的英文单词
+ * @param $textarea
+ */
+export function getCursorEnglishWord($textarea) {
+    // TODO 调用后台获取更准确的【猜你想查】
+    const textareaText = $textarea.val();
+    const position = $textarea.prop('selectionStart');
+    let start = position, end = position;
+    // 向前和向后扫描，直到找到单词的边界
+    while (start > 0 && /\S/.test(textareaText[start - 1])) {
+        start--;
+    }
+    while (end < textareaText.length && /\S/.test(textareaText[end])) {
+        end++;
+    }
+    return textareaText.substring(start, end);
+}
+
+/**
+ * 获取光标前方的单词
+ * @param $textarea
+ * @param cursorPosition
+ * @returns {string}
+ */
+export function getBeforeCursorWord($textarea, cursorPosition) {
+    const textareaText = $textarea.val();
+    const position = $textarea.prop('selectionStart');
+    const textBeforeCursor = textareaText.substring(0, position)
+    if (/[\u3040-\u30FF]/.test(textBeforeCursor)) {
+        // 含有假名，说明可能是日语
+        // TODO 暂时先返回光标前的所有文本分析 API
+        return textBeforeCursor;
+    } else {
+        return getCursorEnglishWord(textareaText, textBeforeCursor, cursorPosition, 'before')
+    }
+}
+
+/**
+ * 按下↤时查询光标前方的单词
+ */
+$("#getWordBeforeCursor").on('click', function () {
+    const $input = $('#contextInput');
+    const cursorPosition = $input.prop('selectionStart');
+    const word = getBeforeCursorWord($input, cursorPosition);
+    $('#wordInput').val(word);
+    $('#searchButton').click();
+    $input.focus();
+})
+
+function getCursorWord(text, textCursor, cursorPosition, cursorPattern) {
+    if (/^[a-zA-Z]+$/.test(textCursor) && textCursor.includes(" ")) {
+        // 如果是纯英文
+        let start = cursorPosition, end = cursorPosition;
+        while (start > 0 && /\S/.test(text[start - 1])) {
+            start--;
+        }
+        while (end < text.length && /\S/.test(text[end])) {
+            end++;
+        }
+        return text.substring(start, end);
+    } else {
+        // 不全是英文，且没有空格，基于中英文直接分割单词
+        // 注意【test1中文test2测试】如果光标在【测试】，那么返回应该返回test2
+        const CursorWords = textCursor.match(/[a-zA-Z]+/g)
+        if (CursorWords === null) {
+            // 全中文，直接返回
+            if (cursorPattern === 'after') {
+                // 返回光标后的所有文字
+                return text.substring(cursorPosition, text.length);
+            } else {
+                // 返回光标前的所有文字
+                return text.substring(0, cursorPosition);
+            }
+        } else {
+            if (cursorPattern === 'after') {
+                // 查找光标后的第一个单词
+                return CursorWords[0];
+            } else {
+                // 查找光标前的第一个单词，是提取的单词中的最后一个
+                return CursorWords[CursorWords.length - 1];
+            }
+        }
+    }
+}
+
+/**
+ * 获取光标后方的单词
+ * @param $textarea
+ * @returns {string}
+ */
+export function getAfterCursorWord($textarea) {
+    const textareaText = $textarea.val();
+    const cursorPosition = $textarea.prop('selectionStart');
+    const textAfterCursor = textareaText.substring(cursorPosition, textareaText.length);
+    if (/[\u3040-\u30FF]/.test(textAfterCursor)) {
+        // 含有假名，说明可能是日语
+        // TODO 暂时先返回光标前的所有文本
+        return textAfterCursor;
+    } else {
+        return getCursorWord(textareaText, textAfterCursor, cursorPosition, 'after')
+    }
+}
+
+/**
+ * 按下↦时查询光标后方的单词
+ */
+$("#getWordAfterCursor").on('click', function () {
+    const $input = $('#contextInput');
+    const word = getAfterCursorWord($input)
+    $('#wordInput').val(word);
+    $('#searchButton').click();
+    // 即使没有聚焦到文本区域，也直接进入文本区域，减少用户操作次数
+    $input.focus();
+})
+
+
+/**
+ * 【猜你想查】按钮触发搜索事件
+ */
+$("#want-search-container").on("click", "button.want-search", function () {
+    const wantSearchWord = $(this).val()
+    // TODO 提取下面的方法
+    $("#wordInput").val(wantSearchWord)
+    $('#searchButton').click();
+})
+
+/*
+将语境框内的光标向前移动一个单词
+ */
+$('#moveCursorAfter').on('click', function () {
+    // 获取文本区域的内容
+    const $input = $('#contextInput');
+    // 即使没有聚焦到文本区域，也直接进入文本区域，减少用户操作次数
+    $input.focus();
+    const text = $input.val();
+    // 获取当前光标位置
+    const cursorPosition = $input.prop('selectionStart');
+    // 使用 Segmenter 分割文本
+    // https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+    // 中文 zh 英语 en
+    let langTag = 'en-US'; //
+    if (/[\u3040-\u30FF]/.test(text)) {
+        console.log("日语")
+        langTag = 'ja-JP';
+    }
+    const segmenter = new Intl.Segmenter(langTag, {granularity: 'word'});
+    //const segments = Array.from(segmenter.segment(text));
+    const segments = [...segmenter.segment(text)];
+
+    console.log(segments);
+
+    let wordIndex = 0
+    let charCount = 0;
+
+    for (const segment of segments) {
+        // 通过遍历分词结果判断当前光标所在分词结果
+        const segmentLength = segment.segment.length;
+        if (charCount + segmentLength > cursorPosition) {
+            console.log(` charCount${charCount} cursorPosition${cursorPosition}segmentLength ${segmentLength}`)
+            break;
+        }
+        charCount += segmentLength;
+        wordIndex++;
+    }
+    // 基于当前光标在的分词结果的索引计算出下一个单词的索引
+    const nextWordIndex = wordIndex + 1;
+    if (nextWordIndex >= segments.length) {
+        // 直接移动光标到最后一个字符处
+        console.log("直接移动光标到最后一个字符处")
+        $input[0].setSelectionRange(text.length, text.length);
+    } else {
+        // 移动光标到下一个单词的开头
+        const nextWord = segments[nextWordIndex].segment;
+        const nextWordStart = text.lastIndexOf(nextWord);
+        $input[0].setSelectionRange(nextWordStart, nextWordStart);
+    }
+    $input.focus();
+});
+
+/**
+ * 将语境框内的光标向前移动一个单词
+ */
+$('#moveCursorBefore').on('click', function () {
+    const $input = $('#contextInput');
+    $input.focus();
+    const cursorPosition = $input.prop('selectionStart'); // 获取当前光标位置
+    const text = $input.val(); // 获取文本的完整内容
+
+    // 主流桌面端和移动端设备已经都已兼容该 API
+    // TODO 注意在 Kindle 这样的设备上可能不支持
+    const segmenter = new Intl.Segmenter('ja-JP', {granularity: 'word'});
+    const segments = [...segmenter.segment(text)];
+
+
+    let wordIndex = 0;
+    let charCount = 0;
+
+    // 处理光标在文本开头的情况
+    if (cursorPosition === 0) {
+        return;
+    }
+
+    // 计算当前光标前的单词数量
+    for (const segment of segments) {
+        const segmentLength = segment.segment.length;
+        if (charCount + segmentLength > cursorPosition) {
+            break; // 到达光标位置
+        }
+        charCount += segmentLength;
+        wordIndex++;
+    }
+
+    // 计算光标前一个单词的起始位置
+    if (wordIndex > 0) {
+        const previousWord = segments[wordIndex - 1].segment;
+        const previousWordStart = text.lastIndexOf(previousWord, charCount - previousWord.length);
+        $input[0].setSelectionRange(previousWordStart, previousWordStart); // 设置光标位置
+    }
+
+    // 确保文本框获得焦点
+    $input.focus();
+});
+
 
 /**
  * 基于用户的输入重新渲染链接，渲染后的链接直接指向搜索地址
@@ -33,7 +257,7 @@ export function initCreateLink(link, linkKey) {
 }
 
 /**
- * 根据服务器返回的检查结果渲染对应的链接
+ * 根据服务器返回的检查结果重新渲染对应的链接元素
  * @param url_index 链接的索引，和已经渲染好的 DOM 元素的 ID 保持一致
  * @param status 链接的检查结果
  */
--- static/src/script.js
+++ static/src/script.js
@@ -1,9 +1,11 @@
 import localforage from "localforage";
 import {checkResultInBackend, creatResultLinks, initializeDb} from "static/src/db";
-import {initCreateLink} from "static/src/dom";
+import {getCursorEnglishWord, initCreateLink} from "static/src/dom";
+
 
 /**
- * 数据库相关初始化操作
+ * 从数据库读取搜索链接数据，同时渲染到屏幕上。
+ * 如果是第一次访问，那么读取后
  */
 function checkDb() {
     localforage.config({
@@ -19,7 +21,8 @@ function checkDb() {
                 )
             });
         } else {
-            //  FIXME 重复代码，读取数据库中存储的链接
+            // FIXME 注意不管是否渲染成功，都应该添加自定义的按钮
+            // FIXME 重复代码，读取数据库中存储的链接
             localforage.iterate((value, key) => {
                 // 链接以【1:{}】类似的形式存储
                 if (value !== undefined && /^\d+$/.test(key)) {
@@ -32,23 +35,88 @@ function checkDb() {
     })
 }
 
+
+/**
+ * 在语境框内双击时搜索单词
+ */
+function doubleClickSearch() {
+    // TODO 参数是自定义的按钮代码，注意使用枚举的类型
+    const doubleClickKeyName = "Shift"
+    const $textarea = $('#contextInput');
+    let pressCount = 0;
+    let pressTimeout;
+    $($textarea).on('keydown', function (event) {
+            // TODO 允许自定义按键，区分左右按键
+            if (event.key === doubleClickKeyName) {
+                pressCount++;
+                // 如果已经计数到 2，进行单词查找
+                if (pressCount === 2) {
+                    // 判断是否选中了文本
+                    let wantSearchText = window.getSelection().toString()
+                    if (wantSearchText === "") {
+                        // 如果没有选中文本，那么直接根据空格判断单词边界
+                        wantSearchText = getCursorEnglishWord($textarea);
+                    }
+                    // TODO 分析选中的文本
+                    // 构建搜索按钮
+                    createWantSearchButtons(wantSearchText);
+                    // 重置计数器
+                    pressCount = 0;
+                }
+            }
+            // 防止双击过快
+            clearTimeout(pressTimeout);
+            pressTimeout = setTimeout(() => {
+                pressCount = 0; // 超时后重置计数
+            }, 300);
+        }
+    );
+    $($textarea).on('keyup', function (event) {
+        if (event.key !== doubleClickKeyName) {
+            //释放键时重置计数
+            pressCount = 0;
+        }
+    });
+}
+
+/**
+ * 渲染【猜你想查】按钮
+ * @param wantSearchWord
+ */
+function createWantSearchButtons(wantSearchWord) {
+    // 移除上次添加的按钮
+    $("button.want-search").remove();
+    // TODO 传入的应该一个数组
+    // 渲染用户可能想查的单词的按钮
+    $("#want-search-container").append(`<button class=\"btn btn-primary me-2 btn-sm want-search\" id=\"want\" value=\"${wantSearchWord}\">${wantSearchWord}</button>`)
+    // TODO 反馈按钮，用于向收集尚未收录在非辞書中的单词
+}
+
 $(document).ready(function () {
     checkDb();
 
-    $('#searchButton').on('click', function (e) {
+    $('#searchButton').on('click', function () {
         const word = $('#wordInput').val();
+        // 未输入文字时不搜索
+        if (word === "") {
+            return
+        }
+
         const $resultsList = $('#resultsList');
         // 清空现有的搜索结果
         $resultsList.empty();
-        $('.result-area').show(); // 显示结果区域
+        // 显示结果区域
+        $('.result-area').show();
+        // 先构建搜索链接，链接构建成功后，再向后台提交搜索结果
         creatResultLinks(word, $resultsList).then(
             // TODO 如果用户有不需要检查结果的网站，那么先自动打开一次相关链接
             checkResultInBackend(word).then(
-                // TODO 收到并成功重新渲染了链接
+                // TODO 收到服务器的结果并成功重新渲染了链接
             )
         )
     });
 
+    // 搜索框内按回车触发搜索事件
     $('#wordInput').keydown(function (e) {
         if (e.key === 'Enter') {
             e.preventDefault();
@@ -59,5 +127,15 @@ $(document).ready(function () {
 
     // TODO 筛选超链接
 
-    // TODO 语境框双击键盘坐标
+    // TODO 语境框双击键盘
+    doubleClickSearch()
+
+    // 双击自动查找选中的单词
+    $('#contextInput').on('dblclick', function (event) {
+        const selectedText = window.getSelection().toString();
+
+        // TODO 提取下面的方法
+        $("#wordInput").val(selectedText)
+        $('#searchButton').click();
+    });
 });
