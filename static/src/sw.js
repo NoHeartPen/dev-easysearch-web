6584c253237a73059b8e1f2533891666e0e0583d
John Qing <3104408599@qq.com>
2025-04-17 14:20:27 +0900

    chore: comment

--- static/src/sw.js
+++ static/src/sw.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = 'v1'; // 设置 Cache 版本
+const CACHE_VERSION = 'v1';
 const CACHE_NAME = `my-cache-${CACHE_VERSION}`;
-const ALLOWED_PATHS = ['/']; // 请根据需要修改允许的路径
+const ALLOWED_PATHS = ['/'];
 const EASYSEARCH_API = ["/word-analyze", "/full-analyze","/search"]
 
 self.addEventListener('install', event => {
@@ -27,7 +27,8 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName); // 删除旧缓存
+                        // 删除旧缓存
+                        return caches.delete(cacheName);
                     }
                 })
             );
@@ -36,30 +37,28 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-    // 检查请求的协议是否为 http 或 https
+    // TODO 检查请求的协议是否为 http 或 https
     const requestUrl = new URL(event.request.url);
     if (requestUrl.protocol !== 'http:' && requestUrl.protocol !== 'https:') {
-        return; // 直接返回，不处理非http(s)的请求
+        // 直接返回，不处理非http(s)的请求
+        return;
     }
     event.respondWith(
         (async () => {
             try {
-                // 处理特定请求
+                // TODO 处理特定请求
                 if (event.request.url.endsWith('/init-urls')) {
                     const response = await fetch(event.request);
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
                     }
-                    return response; // 返回有效的 Response 对象
+                    return response;
                 }
-
                 // 处理其他请求
                 const response = await fetch(event.request);
                 if (!response || response.status !== 200) {
                     throw new Error('Network response was not ok');
                 }
-
-                // 检查请求是否为 POST，HTML 并在允许的路径内
                 const requestPath = requestUrl.pathname;
                 if (event.request.method === 'POST'
                     && event.request.headers.get('Content-Type').includes('application/json')
@@ -77,18 +76,19 @@ self.addEventListener('fetch', event => {
                     await cache.put(event.request, responseClone);
                     console.log("网络访问成功，更新缓存");
                 }
-                return response; // 返回网络响应
+                return response;
             } catch (error) {
                 console.error('Fetch error:', error);
-                // 如果网络访问失败，尝试从缓存中返回
+                // 如果网络访问失败，尝试返回缓存
                 const cachedResponse = await caches.match(event.request);
                 if (cachedResponse) {
-                    return cachedResponse; // 返回缓存响应
+                    return cachedResponse;
                 }
-                // 检查请求是否为 GET，HTML 并在允许的路径内
+                // TODO 检查请求是否为 GET，HTML 并在允许的路径内
                 const requestPath = requestUrl.pathname;
                 if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html') && ALLOWED_PATHS.includes(requestPath)) {
-                    return caches.match("/"); // 返回离线页面
+                    // 返回离线页面
+                    return caches.match("/");
                 }
             }
         })()
