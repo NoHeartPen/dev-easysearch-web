b5d25ffe527a72ec690890ae2c72271bfd35fd19
noheartpen <3104408599@qq.com>
2025-11-27 21:46:02 +0900

    test: 修改单元测试方法名以提升可读性并精简 assert 逻辑

--- test/backend/test_get.py
+++ test/backend/test_get.py
@@ -6,23 +6,8 @@ from utils.check_result.check_get import async_get_for_url, get_for_url
 
 
 class MyTestCase(unittest.TestCase):
-    # TODO 修改方法名
-    def test_something(self):
-        self.assertEqual(
-            True,
-            get_for_url(
-                "https://www.weblio.jp/content/%E9%A3%9F%E3%81%B9%E3%82%8B",
-                None,
-                "見出し語は見つかりませんでした",
-            ),
-            get_for_url(
-                "https://www.weblio.jp/content/食べる",
-                None,
-                "見出し語は見つかりませんでした。",
-            ),
-        )
-        self.assertEqual(
-            False,
+    def test_not_found(self):
+        self.assertFalse(
             get_for_url(
                 "https://www.weblio.jp/content/XXXXXXXXXXXXXXXXXXXXXXXXXXX",
                 None,
